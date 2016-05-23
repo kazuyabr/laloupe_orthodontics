32746557c42ecc92b4138b26f633216071f97f79
niwi84 <ninon.pelaez@orange.fr>
2016-05-23 17:55:07 +0200

    checkbox pour appareillages

--- src/OrthoBundle/Form/CommandesType.php
+++ src/OrthoBundle/Form/CommandesType.php
@@ -10,7 +10,6 @@ use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 
-
 class CommandesType extends AbstractType
 {
     /**
@@ -26,65 +25,52 @@ class CommandesType extends AbstractType
                 ),
                 'label' => 'Référence Patient'
             ))
-            
             ->add('prenomPatient', 'text', array(
                 'attr' => array(
                     'placeholder' => 'Ex : Jean'
                 ),
                 'label' => 'Prénom Patient'
             ))
-
             ->add('dateretour', 'date', array(
-                'attr' => array(
-                    ),
+                'attr' => array(),
                 'widget' => 'single_text',
 
             ))
-
-            ->add('appareillages', 'entity', array(
+            ->add('appareillages', EntityType::class, array(
                 'class' => 'OrthoBundle:Appareillages',
                 'property' => 'titre_app',
+                'expanded' => 'true',
                 'multiple' => 'false',
-                'query_builder' => function(\Doctrine\ORM\EntityRepository $entityRepository)
-                {
+                'query_builder' => function (\Doctrine\ORM\EntityRepository $entityRepository) {
                     return $entityRepository->triParPoids();
                 }
             ))
             
             ->add('ajoutApp', 'button')
-            
+
             ->add('fidAdj', EntityType::class, array(
                 'class' => 'OrthoBundle:Adjonctions',
                 'property' => 'titre_adj',
                 'multiple' => 'false'
             ))
-
             ->add('ajoutAdj', 'button')
-
             ->add('fidCouleur')
-            
             ->add('fidMotif')
-            
             ->add('comment', 'textarea')
-            
             ->add('testimage', 'file', array(
                 'mapped' => false
             ))
-            
             ->add('testimage1', 'file', array(
                 'mapped' => false,
                 'required' => false
             ))
-            
             ->add('testimage2', 'file', array(
                 'mapped' => false,
                 'required' => false
             ))
-            
             ->add('comment2', 'textarea', array(
                 'required' => false
-            ))
-        ;
+            ));
     }
 
     /**
