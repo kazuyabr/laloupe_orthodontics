bb66ad003c7e930d420c2209eed19877c63aa5cc
OuterAlchemy <contact.thomasmerlin@gmail.com>
2016-05-26 00:40:04 +0200

    Modifications | Merge à venir

--- src/OrthoBundle/Resources/public/js/formulaire.js
+++ src/OrthoBundle/Resources/public/js/formulaire.js
@@ -1,26 +1,11 @@
-
-var monSelectCouleur = $('#commandes_fidCouleur');
-monSelectCouleur.on('change', function () {
-    $('#remplissageCadreCouleur').attr('src', "{{ asset('bundles/ortho/img/couleurs/') }}" + monSelectCouleur.val() + ".png");
-});
-
-
-
-var monSelectMotif = $('#commandes_fidMotif');
-monSelectMotif.on('change', function () {
-    $('#remplissageCadreMotif').attr('src', "{{ asset('bundles/ortho/img/motifs/') }}" + monSelectMotif.val() + ".png");
-})
-
-
-
 $(function() {
     $('#commandes_dateretour').datepicker({
         dateFormat: 'dd-mm-yyyy'
     })
 });
 
-//ajouter class='' à input appareillage
-//$("div#commandes_appareillages").addClass("btn-group");
+// Ajout d'une class sur le label des appareillages pour afficher un bouton
+// Masquage des coches des checkbox
 $("div#commandes_appareillages > label").addClass("btn btn-default");
 $("div#commandes_appareillages > input").hide();
 
