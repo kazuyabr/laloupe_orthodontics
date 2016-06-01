496ed5bf1ec663f8badb535b3f6b97582b3bc002
OuterAlchemy <contact.thomasmerlin@gmail.com>
2016-06-01 08:58:32 +0200

    Commencement de la sécurité | Mise à jour de la DataFixture

--- src/OrthoBundle/DataFixtures/ORM/CabinetsdentairesData.php
+++ src/OrthoBundle/DataFixtures/ORM/CabinetsdentairesData.php
@@ -18,7 +18,7 @@ class CabinetsdentairesData extends AbstractFixture implements OrderedFixtureInt
         $cabinet1->setPlainPassword("SuperAdmin");
         $cabinet1->setEnabled(true);
         $cabinet1->setEmail("SuperAdmin@gmail.com");
-        $cabinet1->setRoles(array('ROLE_CABINET'));
+        $cabinet1->setRoles(array('ROLE_SUPER_ADMIN'));
         $cabinet1->setFidLabo($this->getReference('laboratoire1'));
         $cabinet1->setNomCab("Néo 3D");
         $cabinet1->setAdresseCab("5 Allée des Atlantes");
@@ -32,14 +32,44 @@ class CabinetsdentairesData extends AbstractFixture implements OrderedFixtureInt
         $cabinet2->setPlainPassword("Neo3d");
         $cabinet2->setEnabled(true);
         $cabinet2->setEmail("teamneo3D@gmail.com");
-        $cabinet2->setRoles(array('ROLE_USER'));
+        $cabinet2->setRoles(array('ROLE_TEAMNEO'));
         $cabinet2->setFidLabo($this->getReference('laboratoire1'));
         $cabinet2->setNomCab("Néo 3D");
         $cabinet2->setAdresseCab("5 Allée des Atlantes");
         $cabinet2->setCodepostalCab("28000");
+        $cabinet2->setVilleCab("Chartres");$cabinet2->setCodepostalCab("28000");
         $cabinet2->setVilleCab("Chartres");
         $cabinet2->setTelephoneCab("02.37.90.61.84");
         $manager->persist($cabinet2);
+        
+        
+        $cabinet3 =  new Cabinetsdentaires();
+        $cabinet3->setUsername("Laboratoire1");
+        $cabinet3->setPlainPassword("Laboratoire1");
+        $cabinet3->setEnabled(true);
+        $cabinet3->setEmail("Laboratoire@gmail.com");
+        $cabinet3->setRoles(array('ROLE_LABORATOIRE'));
+        $cabinet3->setFidLabo($this->getReference(('laboratoire1')));
+        $cabinet3->setNomCab("Laboratoire Néo 3D");
+        $cabinet3->setAdresseCab("5 Allée des Atlantes");
+        $cabinet3->setCodepostalCab("28000");
+        $cabinet3->setVilleCab("Chartres");
+        $cabinet3->setTelephoneCab("02.37.90.61.84");
+        $manager->persist($cabinet3);
+
+        $cabinet4 = new Cabinetsdentaires();
+        $cabinet4->setUsername("Cabinet1");
+        $cabinet4->setPlainPassword("Cabinet1");
+        $cabinet4->setEnabled(true);
+        $cabinet4->setEmail("Cabinet@gmail.com");
+        $cabinet4->setRoles(array('ROLE_CABINET'));
+        $cabinet4->setFidLabo($this->getReference(('laboratoire1')));
+        $cabinet4->setNomCab("Cabinet Néo 3D");
+        $cabinet4->setAdresseCab("5 Allée des Atlantes");
+        $cabinet4->setCodepostalCab("28000");
+        $cabinet4->setVilleCab("Chartres");
+        $cabinet4->setTelephoneCab("02.37.90.61.84");
+        $manager->persist($cabinet4);
         
         $manager->flush();
     }
